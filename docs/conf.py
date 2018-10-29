866ed88fb1c0378b2f0e0b031e72872e9e90cc86
rehamFatima <reham@ebi.ac.uk>
2018-10-29 14:10:54 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -68,9 +68,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'MuG - Test Pipelines'
-copyright = '2018, Mark McDowall'
-author = 'Mark McDowall'
+project = 'MuG - BamQC Tool and Pipeline'
+copyright = '2018, Reham Fatima'
+author = 'Reham Fatima'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -123,7 +123,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'MuG-ProcessTestPipelinesdoc'
+htmlhelp_basename = 'MuG-BamQCPipelinesdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -150,8 +150,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'MuG-ProcessTestPipelines.tex', 'MuG - Process Test Pipelines Documentation',
-     'Mark McDowall', 'manual'),
+    (master_doc, 'MuG-BamQCPipelines.tex', 'MuG - Process BamQC Pipelines Documentation',
+     'Reham Fatima', 'manual'),
 ]
 
 
@@ -160,7 +160,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'mug-processtsvpipelines', 'MuG - Process Test Pipelines Documentation',
+    (master_doc, 'mug-processtsvpipelines', 'MuG - Process BamQC Pipelines Documentation',
      [author], 1)
 ]
 
@@ -171,8 +171,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'MuG-ProcessTestPipelines', 'MuG - Process Test Pipelines Documentation',
-     author, 'MuG-ProcessTestPipelines', 'One line description of project.',
+    (master_doc, 'MuG-ProcessBamQCPipelines', 'MuG - Process BamQC Pipelines Documentation',
+     author, 'MuG-ProcessBamQCPipelines', 'Wrapper project for BamQC.',
      'Miscellaneous'),
 ]
 
