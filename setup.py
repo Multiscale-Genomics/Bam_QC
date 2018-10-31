d722d41f114226c36b28b45266837f0d636f8326
rehamFatima <reham@ebi.ac.uk>
2018-10-31 16:12:58 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'numpy', 'h5py', 'pytest', 'scipy', 'matplotlib', 'pysam', 'mock',
-        'bz2file', 'ConfigParser', 'future','pytest'
+        'bz2file', 'ConfigParser', 'future'
     ],
     setup_requires=[
         'pytest-runner',
